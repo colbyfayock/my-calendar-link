5d51c67850f9b79bcb88c6d3774631fef6f2b1d7
Colby Fayock <fayock@gmail.com>
2024-05-06 23:00:26 -0400

    Step 2: Adding Add to Calendar button that opens and closes link menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import { google, outlook, office365, yahoo, ics, CalendarEvent } from "calendar-link";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
   const event: CalendarEvent = {
     title: 'Space Jelly Palooza',
     description: 'The biggest party in the universe.',
@@ -16,23 +19,41 @@ function App() {
 
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <ul>
-        <li>
-          <a href={icsUrl} target="_blank">Add to Apple (iCal)</a>
-        </li>
-        <li>
-          <a href={googleUrl} target="_blank">Add to Google Calendar</a>
-        </li>
-        <li>
-          <a href={office365Url} target="_blank">Add to Office365</a>
-        </li>
-        <li>
-          <a href={outlookUrl} target="_blank">Add to Outlook</a>
-        </li>
-        <li>
-          <a href={yahooUrl} target="_blank">Add to Yahoo</a>
-        </li>
-      </ul>
+      <span className="inline-block relative">
+        <button
+          className="bg-white border border-zinc-600 rounded px-4 py-3 font-bold"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          Add to Calendar
+        </button>
+        <ul className={`${isOpen ? 'opacity-100' : 'opacity-0' } absolute top-full left-0 min-w-full border shadow-lg py-2 px-1 transition-opacity`}>
+          <li>
+            <a href={icsUrl} target="_blank" className="block text-zinc-800 hover:bg-zinc-50 px-4 py-2 whitespace-nowrap">
+              Apple (iCal)
+            </a>
+          </li>
+          <li>
+            <a href={googleUrl} target="_blank" className="block text-zinc-800 hover:bg-zinc-50 px-4 py-2 whitespace-nowrap">
+              Google Calendar
+            </a>
+          </li>
+          <li>
+            <a href={office365Url} target="_blank" className="block text-zinc-800 hover:bg-zinc-50 px-4 py-2 whitespace-nowrap">
+              Office365
+            </a>
+          </li>
+          <li>
+            <a href={outlookUrl} target="_blank" className="block text-zinc-800 hover:bg-zinc-50 px-4 py-2 whitespace-nowrap">
+              Outlook
+            </a>
+          </li>
+          <li>
+            <a href={yahooUrl} target="_blank" className="block text-zinc-800 hover:bg-zinc-50 px-4 py-2 whitespace-nowrap">
+              Yahoo
+            </a>
+          </li>
+        </ul>
+      </span>
     </div>
   )
 }
