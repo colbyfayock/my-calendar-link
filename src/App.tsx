09793471791b3b12b63e216b11a7230c5513ae03
Colby Fayock <fayock@gmail.com>
2024-05-06 22:25:00 -0400

    Step 1: Dynamically generating calendar links

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
+import { google, outlook, office365, yahoo, ics, CalendarEvent } from "calendar-link";
+
 function App() {
+  const event: CalendarEvent = {
+    title: 'Space Jelly Palooza',
+    description: 'The biggest party in the universe.',
+    start: '2024-07-14 10:00:00 +0300',
+    duration: [8, 'hour'],
+  };
+
+  const googleUrl = google(event);
+  const outlookUrl = outlook(event);
+  const office365Url = office365(event);
+  const yahooUrl = yahoo(event);
+  const icsUrl = ics(event);
+
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <ul>
+        <li>
+          <a href={icsUrl} target="_blank">Add to Apple (iCal)</a>
+        </li>
+        <li>
+          <a href={googleUrl} target="_blank">Add to Google Calendar</a>
+        </li>
+        <li>
+          <a href={office365Url} target="_blank">Add to Office365</a>
+        </li>
+        <li>
+          <a href={outlookUrl} target="_blank">Add to Outlook</a>
+        </li>
+        <li>
+          <a href={yahooUrl} target="_blank">Add to Yahoo</a>
+        </li>
+      </ul>
     </div>
   )
 }
